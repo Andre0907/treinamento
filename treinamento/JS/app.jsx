e260335db30c29ee17b10df22af930bfde5e47e1
Andre0907 <andre_lg_guimaraes@estudante.sesisenai.org.br>
2024-12-12 17:58:49 -0300

    Add files via upload

---
+++ treinamento/JS/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Transacao from './transacao';
+import './transacao.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Transacao />
+    </div>
+  );
+}
+
+export default App;
